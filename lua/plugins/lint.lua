4c13f29dde1bb37dd9a266d2d10dee2de3cad8a3
Grzegorz Dzięgielewski <gdziegielewski@novomatic-tech.com>
2025-01-05 00:26:41 +0100

    adds linting on enter and write

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -8,7 +8,7 @@ return {
       zsh = {"shellcheck"},
       yaml = {"yamllint"},
     }
-    vim.api.nvim_create_autocmd({"BufWritePost"},
+    vim.api.nvim_create_autocmd({"BufEnter", "BufWritePost"},
       { callback = function() require("lint").try_lint() end }
     )
   end
