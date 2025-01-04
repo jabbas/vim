fd2aa47b0232b774185d4a16d1b363fddd0566af
Grzegorz Dzięgielewski <jabbas@jabbas.eu>
2025-01-05 00:24:55 +0100

    sets parallel update for treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,19 @@
 return {
   "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
     config = function()
-      require 'nvim-treesitter.configs'.setup {
+      require('nvim-treesitter.configs').setup {
         ensure_installed = {
           'c', 'cpp', 'python', 'java', 'kotlin', 'javascript', 'go', 'ruby', 'lua',
-          'dockerfile', 'json', 'yaml', 'go', 'gotmpl', 'groovy', 'helm', 'csv', 'ini', 'vim', 'html', 'htmldjango', 'xml', 'lua', 'make', 'nginx', 'perl', 'php', 'promql', 'regex', 'readline', 'ruby', 'rst', 'scss', 'sql', 'strace', 'swift', 'tcl', 'terraform', 'tsv', 'vim', 'vimdoc', 'xml',
+          'dockerfile', 'json', 'yaml', 'go', 'gotmpl', 'gpg', 'groovy', 'helm', 'csv', 'ini',
+          'vim', 'html', 'htmldjango', 'xml', 'lua', 'make', 'nginx', 'perl', 'php', 'promql',
+          'regex', 'readline', 'ruby', 'rst', 'scss', 'sql', 'strace', 'swift', 'tcl',
+          'terraform', 'tsv', 'vim', 'vimdoc', 'xml',
           'gitcommit', 'gitignore', 'git_rebase', 'git_config', 'gitattributes',
-          'bash', 'awk', 'cmake', 'ninja', 'css', 'markdown', 'make', 'diff', 'javascript', 'clojure', 'gpg', 'ini', 'comment', 'jq',
+          'bash', 'awk', 'cmake', 'ninja', 'css', 'markdown', 'make', 'diff', 'javascript',
+          'clojure', 'gpg', 'ini', 'comment', 'jq', 'passwd', 'properties', 'tsv'
         },
         auto_install = true,
+        sync_install = false,
 
         highlight = {
           enable = true,
