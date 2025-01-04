485f382355d7396d91efb37749d364d7956d5b84
Grzegorz Dzięgielewski <jabbas@jabbas.eu>
2025-01-05 00:25:39 +0100

    adds noice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+
+  config = function()
+    require("noice").setup({
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+      }
+    })
+  end
+}
